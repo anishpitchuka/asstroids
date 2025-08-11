e1f815638dc1dd0db78692bf9298f7fa6466b133
anishpitchuka <anishpitchuka8@gmail.com>
2025-08-11 13:21:26 +0000

    moving the object

--- player.py
+++ player.py
@@ -12,6 +12,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -21,6 +25,12 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             # rotate right
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            #forward
+            self.move(dt)
+        if keys[pygame.K_s]:
+            #backward (reverse thrust)
+            self.move(-dt)
 
 
     def triangle(self):
