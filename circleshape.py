9fbe5ffa292adc729602a9c0cee5faf71cbe1d47
anishpitchuka <anishpitchuka8@gmail.com>
2025-08-12 16:02:45 +0000

    add destruction feature

--- circleshape.py
+++ circleshape.py
@@ -13,9 +13,8 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
-    def collides_with(self, other_circle):
-        distance = self.position.distance_to(other_circle.position)
-        return distance < (self.radius + other_circle.radius)
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
 
     def draw(self, screen):
         # sub-classes must override
--- main.py
+++ main.py
@@ -1,23 +1,17 @@
+import sys
 import pygame
 from constants import *
 from player import Player
-from asstroid import Asteroid
-from asstroidsfield import AsteroidField
-from shot import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
-
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
-
-    # Create game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
-    # Sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -25,41 +19,42 @@ def main():
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
-
-
-    # Set static containers
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable,)
-
-    # Create game objects
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    # Game loop
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # Update all updatables
         updatable.update(dt)
 
-        # Collision check: Player vs Asteroids
         for asteroid in asteroids:
-            if player.collides_with(asteroid):
+            if asteroid.collides_with(player):
                 print("Game over!")
-                pygame.quit()
-                raise SystemExit
+                sys.exit()
 
-        # Draw everything
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill("black")
-        for sprite in drawable:
-            sprite.draw(screen)
 
-        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
